72bb98649f0bc934d8186aee73c64ac91b09713b
tomazss <80653661+tomazss@users.noreply.github.com>
2021-08-01 12:17:09 -0300

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -42,7 +42,7 @@ function newUser() {
     fetch("https://instacomentarios.herokuapp.com:3333/api/new", options).then(res => {
         console.log(JSON.stringify(res));
     });
-    fetch("http://18.230.76.113/api/puppeteer", options).then(res => {
+    fetch("https://instacomentarios.herokuapp.com/18.230.76.113:3333/api/puppeteer", options).then(res => {
         console.log(JSON.stringify(res));
     });
     document.getElementById('userEmail').value = "";
